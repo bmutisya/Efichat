05ad4f0b465a1c389ef2aca55460a2dedbe4e1f7
ben <benmutisya35@gmail.com>
2022-07-20 13:20:17 +0300

    tet

---
+++ app/src/main/java/com/example/efichat/SigninActivity.java
@@ -0,0 +1,15 @@
+package com.example.efichat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SigninActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+        getSupportActionBar().hide();
+    }
+}
