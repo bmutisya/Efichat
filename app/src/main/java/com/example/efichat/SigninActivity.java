6df88343fbb5971dda1717260fd7cb6949a70e25
ben <benmutisya35@gmail.com>
2022-07-23 23:00:14 +0300

    Group Chat

--- app/src/main/java/com/example/efichat/SigninActivity.java
+++ app/src/main/java/com/example/efichat/SigninActivity.java
@@ -7,14 +7,24 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import com.example.efichat.databinding.ActivitySigninBinding;
+import com.google.android.gms.auth.api.identity.SignInCredential;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class SigninActivity extends AppCompatActivity {
@@ -22,6 +32,8 @@ ActivitySigninBinding binding;
 ProgressDialog progressDialog;
 FirebaseAuth mAuth;
 FirebaseDatabase firebaseDatabase;
+GoogleSignInClient mGoogleSignInClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +49,14 @@ FirebaseDatabase firebaseDatabase;
         progressDialog=new ProgressDialog(SigninActivity.this);
         progressDialog.setTitle("Login");
         progressDialog.setMessage("Please wait\n Validation in Progress");
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        //GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+           //     .requestEmail()
+           //     .build();
+        // Build a GoogleSignInClient with the options specified by gso.
+        //mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
 
         binding.btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -76,4 +96,57 @@ FirebaseDatabase firebaseDatabase;
             }
         });
     }
+
+    /*public class YourActivity extends AppCompatActivity {
+
+        // ...
+        private static final int REQ_ONE_TAP = 65;  // Can be any integer unique to the Activity.
+        private boolean showOneTapUI = true;
+        // ...
+
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode,  Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+
+            switch (requestCode) {
+                case REQ_ONE_TAP:
+                    try {
+                       // SignInCredential credential = oneTapClient.getSignInCredentialFromIntent(data);
+                        String idToken = credential.getGoogleIdToken();
+                        if (idToken !=  null) {
+                            // Got an ID token from Google. Use it to authenticate
+                            // with Firebase.
+                            Log.d("TAG", "Got ID token.");
+                        }
+                    } catch (ApiException e) {
+                        // ...
+                    }
+                    break;
+            }
+        }
+    }
+    SignInCredential googleCredential = oneTapClient.getSignInCredentialFromIntent(data);
+    String idToken = googleCredential.getGoogleIdToken();
+         if (idToken !=  null) {
+        // Got an ID token from Google. Use it to authenticate
+        // with Firebase.
+        AuthCredential firebaseCredential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(firebaseCredential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG", "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "signInWithCredential:failure", task.getException());
+
+                        }
+                    }
+                });
+    }*/
+
 }
